ad7117de194a2fa73103fa43c891168e90da8db0
Analaissa <laissacoutinho55@gmail.com>
2025-11-26 16:14:16 -0300

    aula11

---
+++ aula 11/script.js
@@ -0,0 +1,23 @@
+// var nomeserie = "la casa de papel";
+// var categoriaserie ="ação"
+// var descricaoserie ="umroubo ao banco"
+// var anoserie ="2017"
+
+// var lacasadepapel = ["la casa de papel","ação","um roubo ao banco","2017"]
+
+class serie{
+    nome;
+    logo;
+    categorias;
+    ntemporada;
+    nEpisodios;
+    desc;
+}
+
+var lacasadepapel = new serie();
+lacasadepapel.nome ="la casa de papel"
+lacasadepapel.logo =
+lacasadepapel.categoria ="ação,suspense,drama,crime"
+lacasadepapel.ntemporada = 5
+lacasadepapel.nepisodios = 41
+lacasadepapel.desc = "la casa de papel gira em torno de um grupo de oito ladrões"
---
+++ aula 11/sistemafilmes.js
@@ -0,0 +1,20 @@
+class filme {
+    // chama e organiza o parenteses
+  constructor(nome , logo, categoria, desc){
+    this.nome = nome
+    this.logo = logo
+    this.categoria = categoria
+    this.desc = desc
+  }
+}
+
+var todosfilmes =[]
+todosfilmes.push(new filme)
+todosfilmes.push(new filme)
+todosfilmes.push(new filme)
+
+
+funci
+for(var i - 0; i < todosfilmes.length;i++){
+    document.querySelector("body").inneshtml +-´
+}
